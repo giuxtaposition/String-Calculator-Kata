613c22f73d8835ebc6177a83fd0e5ca7d565e129
giuxtaposition <yg97.cs@gmail.com>
2021-10-13 19:46:23 +0200

    added support different delimiters test and broke multiple numbers with new lines test

--- test/string-calculator.spec.ts
+++ test/string-calculator.spec.ts
@@ -8,7 +8,8 @@ import { StringCalculator } from '../src/index'
 // - [x] add with multiple numbers
 // - [x] extract numbers from string
 // - [x] sum numbers from array of numbers
-// - [ ] handle new lines between numbers
+// - [x] handle new lines between numbers
+// - [ ] support different delimiters
 
 describe('add numbers in string ', () => {
   it('no numbers', () => {
@@ -36,10 +37,16 @@ describe('add numbers in string ', () => {
   })
 
   it('multiple numbers with new lines', () => {
-    const string: string = '1\n2,3'
+    const string: string = '1\n21,3'
     const otherString: string = '1,\n'
     const stringCalculator = new StringCalculator()
-    expect(stringCalculator.add(string)).to.equal(6)
+    expect(stringCalculator.add(string)).to.equal(25)
     expect(stringCalculator.add(otherString)).to.equal(1)
+  })
+
+  it('support different delimiters', () => {
+    const string: string = '//;\n1;23'
+    const stringCalculator = new StringCalculator()
+    expect(stringCalculator.add(string)).to.equal(24)
   })
 })
