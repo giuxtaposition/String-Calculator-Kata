4e887c31bb45b567c7f3503d35c4c59f6cba04c1
giuxtaposition <yg97.cs@gmail.com>
2021-10-13 18:53:07 +0200

    modified addition with 0 numbers test

--- test/string-calculator.spec.ts
+++ test/string-calculator.spec.ts
@@ -1,14 +1,15 @@
-import { expect } from "chai";
-import { StringCalculator } from "../src/index";
+import { expect } from 'chai'
+import { StringCalculator } from '../src/index'
 
 /*
 [] int add(String numbers)
 The string argument can contain 0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0) for example "" or "1" or "1,2"
 */
 
-describe("add numbers in string ", function () {
-  it("no numbers", () => {
-    const string: string = "";
-    expect(StringCalculator.add(string)).to.equal(0);
-  });
-});
+describe('add numbers in string ', function () {
+  it('no numbers', () => {
+    const string: string = ''
+    const stringCalculator = new StringCalculator()
+    expect(stringCalculator.add(string)).to.equal(0)
+  })
+})
