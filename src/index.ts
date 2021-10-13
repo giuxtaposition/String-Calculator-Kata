bc6a0e61887de0020c7360e255965634b9117705
giuxtaposition <yg97.cs@gmail.com>
2021-10-13 19:00:11 +0200

    made addition with 2 numbers test pass

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 export class StringCalculator {
   add(numbers: string): number {
-    return numbers === '' ? 0 : 1
+    if (!numbers.length) {
+      return 0
+    } else if (numbers.length === 1) {
+      return parseInt(numbers)
+    } else {
+      return parseInt(numbers[0]) + parseInt(numbers[2])
+    }
   }
 }
